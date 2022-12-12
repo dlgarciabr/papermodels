67a9cd322fb4d345ceea86f12df47462cc26c014
Daniel Garcia <dlgarciabr@gmail.com>
2022-12-12 17:10:11 +0000

    crated item category crud

---
+++ src/pages/categories/[categoryId]/edit.tsx
@@ -0,0 +1,84 @@
+import { Suspense } from "react";
+import { Routes } from "@blitzjs/next";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useQuery, useMutation } from "@blitzjs/rpc";
+import { useParam } from "@blitzjs/next";
+
+import Layout from "src/core/layouts/Layout";
+import getCategory from "src/categories/queries/getCategory";
+import updateCategory from "src/categories/mutations/updateCategory";
+import { CategoryForm, FORM_ERROR } from "src/categories/components/CategoryForm";
+
+export const EditCategory = () => {
+  const router = useRouter();
+  const categoryId = useParam("categoryId", "number");
+  const [category, { setQueryData }] = useQuery(
+    getCategory,
+    { id: categoryId },
+    {
+      // This ensures the query never refreshes and overwrites the form data while the user is editing.
+      staleTime: Infinity,
+    }
+  );
+  const [updateCategoryMutation] = useMutation(updateCategory);
+
+  return (
+    <>
+      <Head>
+        <title>Edit Category {category.id}</title>
+      </Head>
+
+      <div>
+        <h1>Edit Category {category.id}</h1>
+        <pre>{JSON.stringify(category, null, 2)}</pre>
+
+        <CategoryForm
+          submitText="Update Category"
+          // TODO use a zod schema for form validation
+          //  - Tip: extract mutation's schema into a shared `validations.ts` file and
+          //         then import and use it here
+          // schema={UpdateCategory}
+          initialValues={category}
+          onSubmit={async (values) => {
+            try {
+              const updated = await updateCategoryMutation({
+                id: category.id,
+                ...values,
+              });
+              await setQueryData(updated);
+              await router.push(Routes.ShowCategoryPage({ categoryId: updated.id }));
+            } catch (error: any) {
+              console.error(error);
+              return {
+                [FORM_ERROR]: error.toString(),
+              };
+            }
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+const EditCategoryPage = () => {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <EditCategory />
+      </Suspense>
+
+      <p>
+        <Link href={Routes.CategoriesPage()}>
+          <a>Categories</a>
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+EditCategoryPage.authenticate = true;
+EditCategoryPage.getLayout = (page) => <Layout>{page}</Layout>;
+
+export default EditCategoryPage;
