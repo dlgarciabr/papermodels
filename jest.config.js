31697289bc203cb2d67cdea17f3d42de548d57f4
Daniel Garcia <dlgarciabr@gmail.com>
2022-12-12 19:53:02 +0000

    test

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,14 @@ const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
   globalSetup: "<rootDir>/src/jestGlobalSetup.ts",
   setupFiles: ["<rootDir>/src/setupTests.js"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
