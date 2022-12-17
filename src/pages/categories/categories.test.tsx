2b0551550023759a6effea033aa4bbff3d8966f1
Daniel Garcia <dlgarciabr@gmail.com>
2022-12-17 11:55:27 +0000

    improved tests

--- src/pages/categories/categories.test.tsx
+++ src/pages/categories/categories.test.tsx
@@ -1,24 +1,75 @@
 import { expect, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
 
-import { render } from "test/utils";
+import { render, screen } from "test/utils";
 import CategoriesPage from ".";
 
 // global arrange
+const categories = [
+  {
+    id: 1,
+    name: "Airplanes",
+  },
+  {
+    id: 2,
+    name: "Cars",
+  },
+  {
+    id: 3,
+    name: "Houses",
+  },
+  {
+    id: 4,
+    name: "Animals",
+  },
+  {
+    id: 5,
+    name: "Trains",
+  },
+  {
+    id: 6,
+    name: "Emergency places",
+  },
+  {
+    id: 7,
+    name: "Boats&Ships",
+  },
+  {
+    id: 8,
+    name: "Stores",
+  },
+  {
+    id: 9,
+    name: "Service building",
+  },
+  {
+    id: 10,
+    name: "Plants",
+  },
+  {
+    id: 11,
+    name: "Miscelaneus",
+  },
+];
+
+// const testMock = () => {
 vi.mock("@blitzjs/rpc", () => ({
   useMutation: () => [],
   usePaginatedQuery: (queryFn: any, params: any, options: any) => [
     {
-      categories: [],
+      categories: categories.slice(0, 10),
+      // categories,
       nextPage: {
-        take: 0,
+        take: 10,
         skip: 0,
       },
-      hasMore: false,
-      count: 0,
+      hasMore: true,
+      count: 11,
     },
     {},
   ],
 }));
+// }
 
 vi.mock("src/categories/queries/getCategories", () => {
   const resolver = vi.fn() as any;
@@ -28,10 +79,42 @@ vi.mock("src/categories/queries/getCategories", () => {
 });
 
 describe("Category", () => {
-  test("Open Category list", () => {
+  test("Open Category list with items", () => {
+    // arrange
+
+    // act
+    render(<CategoriesPage />);
+
+    // assert
+
+    expect(screen.getByRole("link", { name: "Create Category" })).toBeInTheDocument();
+
+    expect(screen.getByRole("link", { name: categories[0]?.name })).toBeInTheDocument();
+  });
+
+  test("Open Category list and navigate through pages", async () => {
     // arrange
     render(<CategoriesPage />);
 
-    expect(true).toBeTruthy();
+    vi.mock("@blitzjs/rpc", () => ({
+      useMutation: () => [],
+      usePaginatedQuery: (queryFn: any, params: any, options: any) => [
+        {
+          categories: categories.slice(11),
+          nextPage: {
+            take: 1,
+            skip: 0,
+          },
+          hasMore: true,
+          count: 11,
+        },
+        {},
+      ],
+    }));
+    // act
+    await userEvent.click(screen.getByRole("button", { name: "Next" }));
+
+    // assert
+    expect(screen.getByRole("link", { name: categories[11]?.name })).toBeInTheDocument();
   });
 });
