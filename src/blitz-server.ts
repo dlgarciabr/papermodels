90e0f9cb88376660787ca67459253306844aa6b3
Daniel Garcia <dlgarciabr@gmail.com>
2022-12-20 16:09:28 +0000

    structure changed

--- src/blitz-server.ts
+++ src/blitz-server.ts
@@ -1,17 +1,22 @@
-import { setupBlitzServer } from "@blitzjs/next"
-import { AuthServerPlugin, PrismaStorage } from "@blitzjs/auth"
-import { simpleRolesIsAuthorized } from "@blitzjs/auth"
-import { BlitzLogger } from "blitz"
-import db from "db"
-import { authConfig } from "./blitz-client"
+import { setupBlitzServer } from '@blitzjs/next';
+import { AuthServerPlugin, PrismaStorage } from '@blitzjs/auth';
+import { simpleRolesIsAuthorized } from '@blitzjs/auth';
+import { BlitzLogger } from 'blitz';
+import db from 'db';
+import { authConfig } from './blitz-client';
+import type { BlitzCliConfig } from 'blitz';
 
 export const { gSSP, gSP, api } = setupBlitzServer({
   plugins: [
     AuthServerPlugin({
       ...authConfig,
       storage: PrismaStorage(db),
-      isAuthorized: simpleRolesIsAuthorized,
-    }),
+      isAuthorized: simpleRolesIsAuthorized
+    })
   ],
-  logger: BlitzLogger({}),
-})
+  logger: BlitzLogger({})
+});
+
+export const cliConfig: BlitzCliConfig = {
+  customTemplates: '.templates'
+};
