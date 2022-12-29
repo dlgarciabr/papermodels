56babb433f38a18e99354bd25c34c1aa7850c248
Daniel Garcia <dlgarciabr@gmail.com>
2022-12-29 14:04:15 +0000

    created a data seed

--- db/seeds.ts
+++ db/seeds.ts
@@ -9,9 +9,47 @@ import db from 'db';
  * to easily generate realistic data.
  */
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  await db.$queryRaw`ALTER SEQUENCE \"public\".\"Category_id_seq\" RESTART WITH 1`;
+  await db.$queryRaw`ALTER SEQUENCE \"public\".\"Item_id_seq\" RESTART WITH 1`;
+  await db.$queryRaw`TRUNCATE TABLE \"public\".\"Category\" CASCADE;`;
+
+  const categories = [
+    { name: 'Emergency services' },
+    {
+      name: 'Service buildings',
+      items: [
+        {
+          name: 'Vet clinic',
+          files: [
+            {
+              id: 'images/vet_clinic.jpg',
+              type: 'scheme'
+            }
+          ]
+        }
+      ]
+    },
+    { name: 'Ships & boats' }
+  ];
+
+  categories.forEach(async (category) => {
+    const categoryCreated = await db.category.create({ data: { name: category.name, description: category.name } });
+    category.items?.forEach(async (item) => {
+      await db.item.create({
+        data: {
+          name: item.name,
+          description: item.name,
+          categoryId: categoryCreated.id,
+          files: {
+            create: item.files.map(({ id, type }) => ({
+              id,
+              type
+            }))
+          }
+        }
+      });
+    });
+  });
 };
 
 export default seed;
