46c1762b9a3fc9563afc432b8acffe654a957735
Daniel Garcia <dlgarciabr@gmail.com>
2023-01-19 11:29:52 +0000

    test git ci

--- src/pages/api/dbKeepAlive/index.page.ts
+++ src/pages/api/dbKeepAlive/index.page.ts
@@ -1,17 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import https from 'https';
 
-const pingDB = () => {
-  try {
-    https.get('https://webhook.site/1167836f-8474-4b11-9604-add5273e67a1');
-  } catch (error) {
-    console.error(error);
-  }
-  setTimeout(() => pingDB(), 60000);
-};
+const pingDB = async () =>
+  new Promise<void>((resolve) => {
+    try {
+      https.get('https://webhook.site/1167836f-8474-4b11-9604-add5273e67a1', (_res) => {
+        resolve();
+      });
+    } catch (error) {
+      console.error(error);
+      resolve();
+    }
+    setTimeout(() => pingDB(), 60000);
+  });
 
-export default function handler(request: NextApiRequest, response: NextApiResponse) {
-  pingDB();
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  await pingDB();
   response.status(200).json({
     body: request.body,
     query: request.query,
