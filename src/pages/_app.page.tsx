8f54149d7b428208bdd0cb3ba2f3a5cc929b698b
Daniel Garcia <dlgarciabr@gmail.com>
2023-01-19 14:58:32 +0000

    test git ci

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -17,8 +17,18 @@ const RootErrorFallback = ({ error }: ErrorFallbackProps) => {
   }
 };
 
+const dbKeepAlive = async () => {
+  if (typeof location !== 'undefined') {
+    await fetch(`${location.href}api/dbKeepAlive`);
+    setTimeout(() => dbKeepAlive(), 60000);
+  }
+};
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const getLayout = Component.getLayout || ((page) => page);
+  if (process.env.NODE_ENV === 'development') {
+    void dbKeepAlive();
+  }
   return (
     <>
       <ErrorBoundary FallbackComponent={RootErrorFallback}>{getLayout(<Component {...pageProps} />)}</ErrorBoundary>
--- src/pages/api/dbKeepAlive/index.page.ts
+++ src/pages/api/dbKeepAlive/index.page.ts
@@ -2,7 +2,8 @@ import db from 'db';
 import { api } from 'src/blitz-server';
 
 export default api(async (_req, res, _ctx) => {
-  await db.$connect();
+  // await db.$connect();
+  await db.$queryRaw`SELECT 1`;
   console.log('db alive!');
   res.status(200).send({});
 });
