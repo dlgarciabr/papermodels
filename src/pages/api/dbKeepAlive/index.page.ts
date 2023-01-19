4dc0417463cf39ad7b72085a26684a06cb77f860
Daniel Garcia <dlgarciabr@gmail.com>
2023-01-19 14:22:18 +0000

    test git ci

--- src/pages/api/dbKeepAlive/index.page.ts
+++ src/pages/api/dbKeepAlive/index.page.ts
@@ -1,16 +1,8 @@
 import db from 'db';
 import { api } from 'src/blitz-server';
 
-const dbKeepAlive = async () => {
-  try {
-    await db.$connect();
-    console.log('db.$connect();');
-  } finally {
-    setTimeout(() => dbKeepAlive(), 30000);
-  }
-};
-
 export default api(async (_req, res, _ctx) => {
-  await dbKeepAlive();
+  await db.$connect();
+  console.log('db alive!');
   res.status(200).send({});
 });
