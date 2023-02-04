bec530c04252b650cfa23bf7b239599be3548bdd
Daniel Garcia <dlgarciabr@gmail.com>
2023-02-04 23:50:51 +0000

    removed quirrel references

--- src/app/mutations/enqueueGreeting.ts
+++
@@ -1,9 +0,0 @@
-// import queueMinutelyCron from '../../pages/api/queueMinutelyCron/index.page';
-
-export default async function enqueueGreeting() {
-  // console.log('mutation queue#########################');
-  // await queueMinutelyCron.enqueue({
-  //   to: 'Sandy Cheeks',
-  //   message: 'Howdy!'
-  // });
-}
