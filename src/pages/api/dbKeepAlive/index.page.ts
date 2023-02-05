4f3d62e257bc810cf9bf316adf04e077471728fe
Daniel Garcia <dlgarciabr@gmail.com>
2023-02-05 09:37:17 +0000

    test

--- src/pages/api/dbKeepAlive/index.page.ts
+++ src/pages/api/dbKeepAlive/index.page.ts
@@ -3,13 +3,14 @@ import db from 'db';
 import { api } from 'src/blitz-server';
 
 const processIntegration = async () => {
+  console.log(`Integration process started at ${new Date().toLocaleDateString()}`);
   setTimeout(() => {
-    console.log('processIntegration');
+    console.log(`Integration process ended at ${new Date().toLocaleDateString()}`);
   }, 5000);
 };
 
 export default api(async (_req, res, _ctx) => {
-  await processIntegration();
+  void processIntegration();
   await db.$queryRaw`SELECT 1`;
   console.log('db alive!');
   res.status(200).send({});
