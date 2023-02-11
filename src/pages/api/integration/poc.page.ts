a20f558181c5366a4bfacceeafc80d62afe38362
Daniel Garcia <dlgarciabr@gmail.com>
2023-02-11 20:14:17 +0000

    playwrite test

--- src/pages/api/integration/poc.page.ts
+++ src/pages/api/integration/poc.page.ts
@@ -1,5 +1,8 @@
 import { api } from 'src/blitz-server';
-import puppeteer from 'puppeteer-serverless';
+// import puppeteer from 'puppeteer-serverless';
+// import chromium from "chrome-aws-lambda"
+// import playwright from "playwright-core"
+import { chromium } from 'playwright-core';
 import path from 'path';
 import fs from 'fs';
 
@@ -8,25 +11,53 @@ const downloadPath = path.resolve('./download');
 export default api(async (req, res, _ctx) => {
   if (req.method === 'GET') {
     try {
-      const browser = await puppeteer.launch({});
-      const page = await browser.newPage();
+      // const browser = await puppeteer.launch({});
+      const browser = await chromium.launch({ headless: true });
+      const context = await browser.newContext();
+      // const browser = await chromium.launch({
+      //   args: chromium.args,
+      //   executablePath:
+      //     process.env.NODE_ENV !== "development"
+      //       ? await chromium.executablePath
+      //       : "/usr/bin/chromium",
+      //   headless: process.env.NODE_ENV !== "development" ? chromium.headless : true,
+      // })
+      const page = await context.newPage();
+
+      const client = await context.newCDPSession(page);
+      await client.send('Page.setDownloadBehavior', {
+        behavior: 'allow',
+        // This path must match the WORKSPACE_DIR in Step 1
+        downloadPath
+      });
 
       await page.goto('https://www.paperdiorama.com/paper-models/work-machines/john-deere-6615-paper-diorama/');
 
-      await page.setViewport({ width: 1080, height: 1024 });
+      // await page.setViewport({ width: 1080, height: 1024 });
 
       const searchResultSelector = 'div.card-body > div > div > a.download-on-click';
       await page.waitForSelector(searchResultSelector);
-      await page._client.send('Page.setDownloadBehavior', {
-        behavior: 'allow',
-        downloadPath: downloadPath
-      });
+      // await page._client.send('Page.setDownloadBehavior', {
+      //   behavior: 'allow',
+      //   downloadPath: downloadPath
+      // });
 
       console.log(`Initializing download of file...`);
 
       await page.click(searchResultSelector);
 
+      // page.on('download', download => {
+      //   console.log(download.suggestedFilename);
+      //   res.status(200);
+      // })
+
+      // const download = await page.waitForEvent("download", {
+      //   timeout: 60000
+      // });
+
       process.stdout.write('downloading');
+
+      // const rs = await download.createReadStream()
 
       await checkDownloadFinished();
       console.log('');
@@ -37,10 +68,10 @@ export default api(async (req, res, _ctx) => {
 
       const rs = fs.createReadStream(`${downloadPath}/${files[0]}`);
 
-      res.setHeader('Content-Disposition', `attachment; ${files[0]}`);
-      rs.pipe(res);
-
-      res.status(200);
+      if (rs) {
+        res.setHeader('Content-Disposition', `attachment; ${files[0]}`);
+        rs.pipe(res);
+      }
     } catch (error) {
       res.status(200).send({ error: error.message });
     }
