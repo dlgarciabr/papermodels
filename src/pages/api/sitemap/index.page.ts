f6235fbf8644516e7d1317fa82a37dfe5c5807b7
Daniel Garcia <dlgarciabr@gmail.com>
2023-03-13 18:21:44 +0000

    WIP

--- src/pages/api/sitemap/index.page.ts
+++ src/pages/api/sitemap/index.page.ts
@@ -3,26 +3,32 @@ import { getServerSideSitemap } from 'next-sitemap';
 import { api } from 'src/blitz-server';
 
 export default api(async (_req, res, _ctx) => {
-  console.log(`[SitemapGenerator] ${new Date().toISOString()} - Sitemap generation process started.`);
+  const now = new Date().toISOString();
+  console.log(`[SitemapGenerator] ${now} - Sitemap generation process started.`);
   try {
     const enabledItems = await db.item.findMany({
       where: {
         status: ItemStatus.enable
       }
     });
+    console.log(`[SitemapGenerator] found ${enabledItems.length} items.`);
     const urls = enabledItems.map((item) => ({
       loc: `${process.env.SITE_URL}/items/${item.id}`,
-      lastmod: new Date().toISOString()
+      lastmod: now
     }));
     urls.splice(0, 0, {
       loc: `${process.env.SITE_URL}`,
-      lastmod: new Date().toISOString()
+      lastmod: now
     });
     const siteMapResponse = await getServerSideSitemap(urls);
-    console.log(`[SitemapGenerator] ${new Date().toISOString()} - Sitemap generation process finished.`);
-    res.setHeader('Content-Type', 'text/xml').status(siteMapResponse.status).send(siteMapResponse.body);
+    console.log(`[SitemapGenerator] ${now} - Sitemap generation process finished.`);
+    res
+      .setHeader('Content-Type', 'text/xml')
+      .setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
+      .status(siteMapResponse.status)
+      .end(siteMapResponse.body);
   } catch (error) {
-    console.log(`[SitemapGenerator] ${new Date().toISOString()} - Sitemap generation process finished with error.`);
+    console.log(`[SitemapGenerator] ${now} - Sitemap generation process finished with error.`);
     res.status(500).send({ ...error });
   }
 });
